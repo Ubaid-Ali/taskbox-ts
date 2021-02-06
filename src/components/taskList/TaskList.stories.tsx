9eeeb20ab55c0fbaf4df1035632a1b2b4591353b
Ubaid-Ali <syedubaidali2020@gmail.com>
2021-02-07 02:08:45 +0500

    Install redux & react-redux again

--- src/components/taskList/TaskList.stories.tsx
+++ src/components/taskList/TaskList.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Story, Meta} from '@storybook/react';
+import { Story, Meta } from '@storybook/react';
 
 import TaskList, { TaskListPropsInterface } from "./TaskList";
 import * as TaskStories from "../task/Task.stories";
